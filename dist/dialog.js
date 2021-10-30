a29d1aee6dbd0abac39cceef45ab3fa872e378cf
Tonyeli Tay <taytonyeli@gmail.com>
2021-10-30 03:02:57 +0000

    Add custom dialog html and replace alert

--- dist/dialog.js
+++ dist/dialog.js
@@ -3,10 +3,25 @@
 class MetaDialog {
     constructor(message = "You have not passed a message") {
         this.message = message;
+
     }
 
     init() {
-        window.alert(`${this.message}`);
+        var dialogHtml =
+            `<div class="meta-dialog-container">
+        <div class="meta-dialog">
+            <div class="meta-dialog-content">
+                <span class="meta-dialog-msg">${this.message}</span>
+                <div class="meta-dialog-actions">
+                    <button class="meta-dialog-button">Yes</button>
+                    <button class="meta-dialog-button">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>`;
+        var dialogElement = document.createElement("div");
+        dialogElement.innerHTML = dialogHtml;
+        document.body.appendChild(dialogElement);
     }
 }
 
