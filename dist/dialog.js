e5421727155730eec954cd084d66fac59bd7e4d9
Tonyeli Tay <taytonyeli@gmail.com>
2021-10-30 02:30:54 +0000

    add ability to create and display dialog

--- dist/dialog.js
+++ dist/dialog.js
@@ -1,6 +1,19 @@
 // TO-DO: logic for creating and displaying dialog
+
+class MetaDialog {
+    constructor(message = "You have not passed a message") {
+        this.message = message;
+    }
+
+    init() {
+        window.alert(`${this.message}`);
+    }
+}
+
+
+
+
 // TO-DO: logic for interacting with dialog buttons
 // TO-DO: logic for displaying confirmation message
 // TO-DO: html for dialog
 // TO-DO: styling for dialog
-console.log("we have arrived");
